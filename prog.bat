6ad9e06a66121e9e0865736f69e6c3b7b70b91ec
Stefan Fischer <sf@hardcoding.net>
2025-04-05 16:29:46 +0900

    updated programming batch file

--- prog.bat
+++ prog.bat
@@ -1,5 +1,4 @@
 @echo off
-STM32_Programmer_CLI -c port=SWD ap=0 -e all
-STM32_Programmer_CLI -c port=SWD ap=0 -w .\FanControl\build\VisualGDB\Release\FanControl.bin
-STM32_Programmer_CLI -c port=SWD ap=0 -v fast
-STM32_Programmer_CLI -c port=SWD ap=0 -w32 0x08007800 0xAABBCC%1
+STM32_Programmer_CLI -c port=SWD reset=HWrst -e all
+STM32_Programmer_CLI -c port=SWD reset=HWrst -w .\FanControl\build\VisualGDB\Release\FanControl.bin 0x08000000 -v
+STM32_Programmer_CLI -c port=SWD reset=HWrst -w32 0x08007800 0xAABBCC%1 -v
